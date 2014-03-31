84a1bbb3465873bef0580272547030761e35a820
Kimberley Manning <solitaira@gmail.com>
2014-03-31 23:50:54 +1000

    working diamond topology

--- code/topos/diamond.py
+++ code/topos/diamond.py
@@ -6,64 +6,58 @@
          |
          s2
        /    \
-h1 -- s1    s3 -- h2
+h1 -- s1    s4 -- h2
        \    /
-         s4
+         s3
 
 When run directly, connects to remote controller already running
 """
 
 from mininet.net import Mininet
+from mininet.node import RemoteController
 from mininet.cli import CLI
-from mininet.topo import Topo
-from mininet.log import lg
-from mininet.node import Node, RemoteController
-from mininet.util import dumpNodeConnections
-from mininet.link import Link
+from mininet.log import setLogLevel, info
 
-class DiamondTopo(Topo):
-    def __init__(self):
-	Topo.__init__(self)
 
-	# add hosts and switches
-	h = [self.addHost('h{}'.format(i+1)) for i in xrange(2)]
-	s = [self.addSwitch('s{}'.format(i+1)) for i in xrange(4)]
+def makeHost(i):
+    label = 'h{}'.format(i)
+    ip = '10.0.0.{}'.format(i)
+    full = '{:012x}'.format(i)
+    mac = ':'.join(full[j:j+2] for j in xrange(0,12,2))
+    opts = {'ip':ip, 'mac':mac}
+    return (label, opts)
 
-	# link them up
-	self.addLink('h1','s1')
-	self.addLink('h2','s3')
+
+def makeSwitch(i):
+    label = 's{}'.format(i)
+    return (label, {})
+
 
 def makeDiamond():
-    topo = DiamondTopo()
-    network = Mininet(topo, controller=RemoteController)
-    return network
+    net = Mininet(controller=RemoteController)
+    c = net.addController('c0')
 
-def testDiamond(net):
+    # add hosts
+    hosts = [makeHost(i+1) for i in xrange(2)]
+    h = [0] + [net.addHost(label, **opts) for label,opts in hosts]
 
-    # configure network
-    switch = net['s1']
-    ip = '10.123.123.1/32'
-    routes = ['10.0.0.0/24']
+    # add switches
+    switches = [makeSwitch(i+1) for i in xrange(4)]
+    s = [0] + [net.addSwitch(label, **opts) for label,opts in switches]
 
-    # start network
-    root = Node('root', inNamespace=False)
-    intf = Link(root, switch).intf1
-    root.setIP(ip, intf=intf)
+    # link them up
+    net.addLink(h[1],s[1])
+    net.addLink(h[2],s[4])
+    net.addLink(s[1],s[2])
+    net.addLink(s[1],s[3])
+    net.addLink(s[2],s[4])
+    net.addLink(s[3],s[4])
+
     net.start()
-    for route in routes:
-        root.cmd( 'route add -net ' + route + ' dev ' + str( intf ) )
-
-    # start tests
-    print "Dumping host connections"
-    dumpNodeConnections(net.hosts)
-    print "Testing network connectivity"
-    #net.pingAll()
     CLI(net)
     net.stop()
 
+
 if __name__ == '__main__':
-    lg.setLogLevel('info')
-    net = makeDiamond()
-    testDiamond(net)
-else:
-    topos = {'diamond': (lambda: DiamondTopo())}
+    setLogLevel('info')
+    makeDiamond()
