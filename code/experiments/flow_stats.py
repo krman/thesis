e60e6d1651f74e9d566d7ed575711fa3afefa15f
Kimberley Manning <solitaira@gmail.com>
2014-04-11 23:21:55 +1000

    make experiments a module too

--- code/experiments/flow_stats.py
+++ code/experiments/flow_stats.py
@@ -4,10 +4,6 @@
 First stats experiment - do flow stats reflect the actual traffic on the network?
 """
 
-import sys
-import subprocess
+from topos import diamond
 
-try:
-    retval = subprocess.call("../topos/diamond.py")
-except OSError as e:
-    print >>sys.stderr, "Failed to start Mininet:", e
+diamond.makeDiamond()
