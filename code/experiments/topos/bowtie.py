ea970e3a89a6e1901ecbc1f4d73c0f15581be55f
Kimberley Manning <solitaira@gmail.com>
2014-05-14 10:53:49 +1000

    add commands to topo scripts

--- code/experiments/topos/bowtie.py
+++ code/experiments/topos/bowtie.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python
 
-""" Diamond topology:
-
-         c
-         |
-         s2
-       /    \
-h1 -- s1    s4 -- h2
-       \    /
-         s3
+""" Bowtie topology:
+    
+		h1
+		|
+         s1     s2
+        / | \  /
+h2 -- s3  |  s4 -- h5 -- h3
+          | /  \
+         s6     s7
+	  |
+	 h4
 
 When run directly, connects to remote controller already running
 """
@@ -31,37 +33,47 @@ def makeSwitch(i):
     return (label, {})
 
 
-def makeDiamond():
+def makeBowtie():
     net = Mininet(controller=RemoteController)
     c = net.addController('c0')
 
     # add hosts
-    hosts = [makeHost(i+1) for i in xrange(2)]
+    hosts = [makeHost(i+1) for i in xrange(4)]
     h = [0] + [net.addHost(label, **opts) for label,opts in hosts]
 
     # add switches
-    switches = [makeSwitch(i+1) for i in xrange(4)]
+    switches = [makeSwitch(i+1) for i in xrange(7)]
     s = [0] + [net.addSwitch(label, **opts) for label,opts in switches]
 
     # link them up
-    TCLink(h[1],s[1], bw=1) # 10 Mbps
-    TCLink(h[2],s[4], bw=1)
-    TCLink(s[1],s[2], bw=1)
+    TCLink(h[1],s[2], bw=1)
+    TCLink(h[2],s[3], bw=1)
+    TCLink(h[3],s[5], bw=1)
+    TCLink(h[4],s[6], bw=1)
+
     TCLink(s[1],s[3], bw=1)
+    TCLink(s[1],s[4], bw=1)
+    TCLink(s[1],s[6], bw=1)
     TCLink(s[2],s[4], bw=1)
-    TCLink(s[3],s[4], bw=1)
+    TCLink(s[4],s[5], bw=1)
+    TCLink(s[4],s[6], bw=1)
+    TCLink(s[4],s[7], bw=1)
+    TCLink(s[5],s[7], bw=1)
 
     return net
 
 
 if __name__ == '__main__':
     setLogLevel('output')
-    net = makeDiamond()
+    net = makeBowtie()
     net.start()
     #CLI(net)
     h1 = net.get('h1')
     h2 = net.get('h2')
+    h3 = net.get('h3')
+    h4 = net.get('h4')
     h1.cmd('ping {} &'.format(h2.IP()))
+    h3.cmd('ping {} &'.format(h4.IP()))
 
     from time import sleep
     sleep(20)
