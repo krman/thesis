2acfcdebdf104ab66ff4faaec5549ce3907d2c86
Kimberley Manning <solitaira@gmail.com>
2014-05-17 12:55:12 +1000

    remove blending problem example

--- code/controllers/multicommodity.py
+++ code/controllers/multicommodity.py
@@ -364,30 +364,6 @@ class Multicommodity:
 	    print v.name, "=", v.varValue
 	print "z =", value(mcf.objective)
 
-    def _solve_blending_problem(self):
-	log.info("calling pulp")
-	log.info(self.mcf)
-	Ingredients = ['CHICKEN', 'BEEF', 'MUTTON', 'RICE', 'WHEAT', 'GEL']
-	costs = {'CHICKEN': 0.013, 'BEEF': 0.008, 'MUTTON': 0.010, 'RICE': 0.002, 'WHEAT': 0.005, 'GEL': 0.001}
-	proteinPercent = {'CHICKEN': 0.100, 'BEEF': 0.200, 'MUTTON': 0.150, 'RICE': 0.000, 'WHEAT': 0.040, 'GEL': 0.000}
-	fatPercent = {'CHICKEN': 0.080, 'BEEF': 0.100, 'MUTTON': 0.110, 'RICE': 0.010, 'WHEAT': 0.010, 'GEL': 0.000}
-	fibrePercent = {'CHICKEN': 0.001, 'BEEF': 0.005, 'MUTTON': 0.003, 'RICE': 0.100, 'WHEAT': 0.150, 'GEL': 0.000}
-	saltPercent = {'CHICKEN': 0.002, 'BEEF': 0.005, 'MUTTON': 0.007, 'RICE': 0.002, 'WHEAT': 0.008, 'GEL': 0.000}
-	prob = LpProblem("The Whiskas Problem", LpMinimize)
-	ingredient_vars = LpVariable.dicts("Ingr",Ingredients,0)
-	prob += lpSum([costs[i]*ingredient_vars[i] for i in Ingredients]), "Total Cost of Ingredients per can"
-	prob += lpSum([ingredient_vars[i] for i in Ingredients]) == 100, "PercentagesSum"
-	prob += lpSum([proteinPercent[i] * ingredient_vars[i] for i in Ingredients]) >= 8.0, "ProteinRequirement"
-	prob += lpSum([fatPercent[i] * ingredient_vars[i] for i in Ingredients]) >= 6.0, "FatRequirement"
-	prob += lpSum([fibrePercent[i] * ingredient_vars[i] for i in Ingredients]) <= 2.0, "FibreRequirement"
-	prob += lpSum([saltPercent[i] * ingredient_vars[i] for i in Ingredients]) <= 0.4, "SaltRequirement"
-	prob.writeLP("WhiskasModel2.lp")
-	prob.solve(GLPK())
-	print "Status:", LpStatus[prob.status]
-	for v in prob.variables():
-	    print v.name, "=", v.varValue
-	print "Total Cost of Ingredients per can = ", value(prob.objective)
-
     def _update_flows(self):
 	#self._get_network()
 	#stats = core.thesis_stats.get_flows()
