be4c38066b52197481960e4d9954b226168432e6
Kimberley Manning <kmanning@gmx.com>
2014-07-07 20:49:17 +1000

    working version of cutoff script

--- code/experiments/cutoff.py
+++ code/experiments/cutoff.py
@@ -17,8 +17,9 @@ from mininet.node import RemoteController
 import networkx as nx
 from fat_tree_graph import fat_tree_graph
 
+
 TOPOLOGY = 'fat_tree'
-OBJECTIVE = 'capacity_spare_max'
+OBJECTIVE = 'shortest_path'
 RESULTS = 'results'
 
 sys.path.append(os.path.abspath('/home/imz/src/pox'))
@@ -29,41 +30,28 @@ obj = importlib.import_module('objectives.' + OBJECTIVE)
 Flow = namedtuple("Flow", "nw_proto nw_src nw_dst tp_src tp_dst")
 Hop = namedtuple("Hop", "dpid port")
 
-# (src,dst):(p1,p2)
-links = {('h1','s1'):(1,1),
-	 ('s1','s2'):(2,1),
-	 ('s1','s3'):(3,1),
-	 ('s2','s4'):(2,2),
-	 ('s3','s4'):(2,3),
-	 ('s4','h2'):(1,1)}
-
-# expected generated rules
-expected = {Flow(nw_proto=None, nw_src='10.0.0.1', nw_dst='10.0.0.2', tp_src=None, tp_dst=None): [Hop(dpid='s1', port=3), Hop(dpid='s3', port=2)]}
-
-
 class TestTopology:
-
-    Flow = namedtuple("Flow", "nw_proto nw_src nw_dst tp_src tp_dst")
-    Hop = namedtuple("Hop", "dpid port")
-    Port = namedtuple("Port", "port_num mac_addr")
-
     def __init__(self, k=4):
 	self.graph = fat_tree_graph(k=k)
 
 
-print "max path len,k,num paths,obj time"
-#for k in [2,4,6,8,10]:
-    #for i in range(5):
+print "max path len,k,num paths,num switches,obj time"
 for c in [10]:
-    for k in [2,4,6,8,10]:
+    for k in [2,4,6,8,10,50,100]:
 	print "{0}, {1},".format(c,k),
 
 	net = TestTopology(k=k)
-	flows = {Flow(nw_src='10.1.3.2', nw_dst='10.3.2.3',nw_proto=None,tp_src=None,tp_dst=None):3}
-	print "{0},".format(len(list(nx.all_simple_paths(net.graph,'h1,3.2','h3,2.3',cutoff=c)))),
+
+	nw_src = '10.{0}.{1}.2'.format(0,k/2)
+	nw_dst = '10.{0}.{1}.2'.format(k-1,k-1)
+	flows = {Flow(nw_src=nw_src, nw_dst=nw_dst,nw_proto=None,tp_src=None,tp_dst=None):3}
+
+	h1 = 'h{0},{1}.2'.format(0,k/2)
+	h2 = 'h{0},{1}.2'.format(k-1,k-1)
+	#paths = nx.all_simple_paths(net.graph, h1, h2)
+	#print "{0}, {1},".format(len(list(paths)), len(net.graph.nodes())),
 
 	start = datetime.now()
-	rules = obj.objective(net, flows, cutoff=c)
+	rules = obj.objective(net, flows)
 	end = datetime.now()
-	#assert rules == expected, "incorrect result"
 	print end-start
