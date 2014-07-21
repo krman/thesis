282b7648124db9ff2220142368681e594a258296
Kimberley Manning <kmanning@gmx.com>
2014-07-21 21:47:16 +1000

    fill out setup.py for mcfpox package

--- code/setup.py
+++ code/setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 from distutils.core import setup, Command
+import codecs
+import os
 
 class PyTest(Command):
     user_options = []
@@ -15,7 +17,33 @@ class PyTest(Command):
         errno = subprocess.call([sys.executable, 'runtests.py'])
         raise SystemExit(errno)
 
+def read(*paths):
+    with codecs.open(os.path.join(*paths), 'r', 'utf-8') as f:
+        return f.read()
 
 setup(
+    name = 'mcfpox',
+    packages = [
+	'mcfpox',
+	'mcfpox.controller',
+	'mcfpox.objectives',
+	'mcfpox.topos',
+	'mcfpox.experiments',
+	'mcfpox.test',
+	'mcfpox.test.controller',
+	'mcfpox.test.objectives',
+	'mcfpox.test.topos'
+    ],
+    version = '0.1.0',
+    description = 'MCF modules for POX',
+    long_description = read('README.rst'),
+    author = 'Kimberley Manning',
+    author_email = 'kmanning@gmx.com',
+    url = 'http://krman.github.io/mcfpox',
+    license = 'Apache License 2.0',
+    classifiers = [
+	'Programming Language :: Python',
+	'License :: OSI Approved :: Apache Software License'
+    ],
     cmdclass = {'test': PyTest},
 )
