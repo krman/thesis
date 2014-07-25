a737eff86ce524fbceccf2de47f7f882b2c132b5
Kimberley Manning <kmanning@gmx.com>
2014-07-25 11:34:12 +1000

    update experiment scripts

--- code/mcfpox/experiments/mock_forwarding_rules.py
+++ code/mcfpox/experiments/mock_forwarding_rules.py
@@ -11,7 +11,10 @@ from mininet.cli import CLI
 from mininet.log import setLogLevel
 from mininet.node import RemoteController
 
-import mcfpox.topos.pentagon as topo
+from mcfpox.topos import pentagon
+from mcfpox.objectives.shortest_path import objective
+from mcfpox.test.objectives.test_shortest_path import pentagon_graph
+from mcfpox.controller.lib import Flow, Hop
 
 
 # Handle SIGINT
@@ -34,26 +37,47 @@ signal.signal(signal.SIGINT, cleanup)
 # Remove any running controllers and/or mininets
 clean = subprocess.call(['sudo', 'mn', '-c'])
 
-# Start POX, running the mcfpox controller module
+# Precalculate switch forwarding rules
+graph = pentagon_graph()
+
+f1 = Flow(6, '10.0.0.1', '10.0.0.2', None, None)
+f2 = Flow(6, '10.0.0.2', '10.0.0.1', None, None)
+flows = {
+    f1: [Hop(1,2), Hop(2,2), Hop(5,1)],
+    f2: [Hop(5,2), Hop(2,1), Hop(1,1)]
+}
+
+rules = objective(graph, flows)
+
+# Start POX, and pass through the pre-installed rules
 objective = 'mcfpox.objectives.shortest_path'
+#"""
 controller = subprocess.Popen(['pox.py', 'log.level', '--CRITICAL', 
 			       'mcfpox.controller.base', 
-			       '--objective='+objective])
+			       '--objective='+objective,
+			       '--preinstall='+str(rules)])
+#"""
 
 # Start mininet with given topology
 setLogLevel('output')
-net = topo.create_net(controller=RemoteController)
-c = net.addController('c0')
+net = pentagon.create_net(controller=RemoteController)
 net.start()
 
 # Wait for network to be discovered
 sleep(15)
 
 # Start flows/perform experiments
+print "Starting flows..."
 h1 = net.get('h1')
 h2 = net.get('h2')
-h1.cmd('ping -c1 ' + str(h2.IP()))
-h2.cmd('ping -c1 ' + str(h1.IP()))
+
+h1.cmd('iperf -s -p 5001 &> h1.server.{0} &'.format(objective))
+for i in [1,2,3,4,5]:
+    print "{0} parallel threads starting".format(i)
+    h2.cmd('iperf -c {0} -p 5001 -P {1} &'.format(str(h1.IP()), i))
+    sleep(12)
+
+print "iperf sessions probably complete"
 
 # Don't exit the script until the controller is killed
 controller.wait()
