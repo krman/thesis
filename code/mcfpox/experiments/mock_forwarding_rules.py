eba3ef863ed5a2506010bf33c6278d5fbda3ad3b
Kimberley Manning <kmanning@gmx.com>
2014-08-07 23:37:32 +1000

    save working experiment script - multiple tcp parallel flows

--- code/mcfpox/experiments/mock_forwarding_rules.py
+++ code/mcfpox/experiments/mock_forwarding_rules.py
@@ -12,6 +12,7 @@ from mcfpox.test.objectives.test_shortest_path import pentagon_graph
 from mcfpox.controller.lib import Flow, Hop
 
 from boilerplate import run_experiment, start_pox
+import matplotlib.pyplot as plt
 
 
 @run_experiment
@@ -44,13 +45,36 @@ def experiment():
     h1 = net.get('h1')
     h2 = net.get('h2')
     
-    h1.cmd('iperf -s -p 5001 &> h1.server.{0}.same_paths &'.format(objective))
-    for i in [1]:
+    received = []
+    import json
+    h1.cmd('iperf3 -s -p 5001 &> h1.server &')
+    flows = [1,3,5,7,9]
+    for i in flows:
 	print "{0} parallel threads starting".format(i)
-	h2.cmd('iperf -c {0} -p 5001 -t 10 -P {1} &> h2.client.same_paths.{1} &'.format(str(h1.IP()), i))
-	sleep(12)
+	h2.cmd('iperf3 -c {0} -p 5001 -b 4m -t 10 -P {1} -J &> h2.client.{1} &'.format(str(h1.IP()), i))
+	sleep(15)
+	f = open('h2.client.{0}'.format(i))
+	j = json.load(f)
+	#print "Sent:", j['end']['sum_sent']['bits_per_second'], "/", 
+	#print "Received:", j['end']['sum_received']['bits_per_second']
+	received.append(j['end']['sum_received']['bits_per_second'])
+	#print "Sent:", j['end']['sum_sent']
+	#print "Received:", j['end']['sum_received']
+	#print "UDP bandwidth:", j['end']['sum']['bits_per_second'], "bps"
+
     
+    sleep(2)
     print "iperf sessions probably complete"
+    y = [i/1e6 for i in [0]+received]
+    print y
+    plt.plot([0]+flows, y, 'g-o')
+    plt.plot(range(10), [20]*10, 'k:')
+    plt.axis([0,9,0,30])
+    plt.xlabel('Number of 4Mbps parallel flows')
+    plt.ylabel('Measured bandwidth between h1 and h2')
+    plt.title('Multiple parallel TCP flows')
+    plt.show()
+    print flows, received
     controller.kill()
 
 
