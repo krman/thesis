233a1558a19fe4fcce2779c6a101849f0f31ace4
Kimberley Manning <kmanning@gmx.com>
2014-09-09 23:27:43 +1000

    make Entrys modifiable

--- code/mcfpox/controller/lib.py
+++ code/mcfpox/controller/lib.py
@@ -7,7 +7,13 @@ from collections import namedtuple
 
 Flow = namedtuple("Flow", "nw_proto nw_src nw_dst tp_src tp_dst")
 Hop = namedtuple("Hop", "dpid port")
-Entry = namedtuple("Entry", "switch id recent total")
+
+class Entry:
+    def __init__(self, switch, id):
+	self.switch = switch
+	self.id = id
+	self.recent = 0
+	self.total = 0
 
 
 def match_to_flow(match):
