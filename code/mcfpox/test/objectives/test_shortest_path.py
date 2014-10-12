772ac6a68613c0e5b7f95c4f09b56829d2dbf887
Kimberley Manning <kmanning@gmx.com>
2014-10-12 17:42:01 +1000

    retab objectives tests

--- code/mcfpox/test/objectives/test_shortest_path.py
+++ code/mcfpox/test/objectives/test_shortest_path.py
@@ -4,22 +4,22 @@ import networkx as nx
 
 def pentagon_graph():
     links = {('h1','s1'):(1,1),
-	     ('s1','s2'):(2,1),
-	     ('s1','s3'):(3,1),
-	     ('s2','s5'):(2,2),
-	     ('s3','s4'):(1,2),
-	     ('s4','s5'):(2,3),
-	     ('s5','h2'):(1,1)}
+             ('s1','s2'):(2,1),
+             ('s1','s3'):(3,1),
+             ('s2','s5'):(2,2),
+             ('s3','s4'):(1,2),
+             ('s4','s5'):(2,3),
+             ('s5','h2'):(1,1)}
 
     UG = nx.Graph()
     UG.add_edges_from(links.keys())
     G = nx.DiGraph(UG)
 
     for a,b in G.edges():
-	try:
-	    G.edge[a][b]['port'] = links[(a,b)][0]
-	except KeyError:
-	    G.edge[a][b]['port'] = links[(b,a)][1]
+        try:
+            G.edge[a][b]['port'] = links[(a,b)][0]
+        except KeyError:
+            G.edge[a][b]['port'] = links[(b,a)][1]
 
     G.node['h1']['ip'] = '10.0.0.1'
     G.node['h2']['ip'] = '10.0.0.2'
@@ -28,22 +28,22 @@ def pentagon_graph():
 
 class TestShortestPath:
     def test_empty_graph_empty_flows(self):
-	assert objective(nx.Graph(), {}) == {}
+        assert objective(nx.Graph(), []) == {}
 
     def test_empty_graph_one_flow(self):
-	f = Flow(nw_proto=6, nw_src='10.0.0.1', nw_dst='10.0.0.2',
-		 tp_src=5001, tp_dst=5002)
-	assert objective(nx.Graph(), {f: 1e6}) == {}
+        f = Flow(nw_proto=6, nw_src='10.0.0.1', nw_dst='10.0.0.2',
+                 tp_src=5001, tp_dst=5002)
+        assert objective(nx.Graph(), [(f,1e6)]) == {}
 
     def test_valid_graph_empty_flows(self):
-	assert objective(pentagon_graph(), {}) == {}
+        assert objective(pentagon_graph(), []) == {}
 
     def test_pentagon_graph_two_flows(self):
-	G = pentagon_graph()
-	f1 = Flow(6, '10.0.0.1', '10.0.0.2', 5001, 5002)
-	f2 = Flow(6, '10.0.0.2', '10.0.0.1', 5003, 5004)
-	expected = {
-	    f1: [Hop(1,2), Hop(2,2), Hop(5,1)], 
-	    f2: [Hop(5,2), Hop(2,1), Hop(1,1)]
-	}
-	assert objective(G, {f1: 1e6, f2: 1e6}) == expected
+        G = pentagon_graph()
+        f1 = Flow(6, '10.0.0.1', '10.0.0.2', 5001, 5002)
+        f2 = Flow(6, '10.0.0.2', '10.0.0.1', 5003, 5004)
+        expected = {
+            f1: [Hop(1,2), Hop(2,2), Hop(5,1)], 
+            f2: [Hop(5,2), Hop(2,1), Hop(1,1)]
+        }
+        assert objective(G, [(f1,1e6), (f2,1e6)]) == expected
